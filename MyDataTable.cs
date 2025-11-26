845a9eec41b063a11536b4c4430ed55f193ce4ed
dhthanh05apr10th <dhthanh.05april10th@gmail.com>
2025-11-26 19:11:37 +0700

    MyDataTable

--- MyDataTable.cs
+++ MyDataTable.cs
@@ -9,12 +9,14 @@ using System.Windows.Forms;
 
 namespace QuanLyBanHang
 {
-    internal class MyDataTable
+    internal class MyDataTable : DataTable
     {
+        //Biến toàn cục
         SqlConnection connection;
         SqlDataAdapter adapter;
         SqlCommand command;
 
+        //Lấy chuỗi kết nối
         public string ConnectionString()
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
@@ -22,6 +24,74 @@ namespace QuanLyBanHang
             builder["Database"] = "QuanLyBanHang";
             builder["Integrated Security"] = true;
             return builder.ConnectionString;
+        }
+
+        //Mở kết nối
+        public bool OpenConnection()
+        {
+            try
+            {
+                if (connection == null)
+                {
+                    connection = new SqlConnection(ConnectionString());
+                }
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                return true;
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+        }
+
+        //Thực thi câu lệnh Select
+        public void Fill(SqlCommand selectCommand)
+        {
+            command = selectCommand;
+            try
+            {
+                command.Connection = connection;
+                adapter = new SqlDataAdapter();
+                adapter.SelectCommand = command;
+
+                this.Clear();
+                adapter.Fill(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể thực thi câu lệnh Sql này!\nLỗi" + ex.Message, "Lỗi truy vấn", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Thực thi câu lệnh Insert, Update, Delete
+        public int Update(SqlCommand insertUpdateDeleteCommand)
+        {
+            int result = 0;
+            SqlTransaction transaction = null;
+            try
+            {
+                transaction = connection.BeginTransaction();
+
+                insertUpdateDeleteCommand.Connection = connection;
+                insertUpdateDeleteCommand.Transaction = transaction;
+                result = insertUpdateDeleteCommand.ExecuteNonQuery();
+
+                this.AcceptChanges();
+                transaction.Commit();
+            }
+            catch(Exception ex)
+            {
+                if(transaction != null)
+                {
+                    transaction.Rollback();
+                }
+                MessageBox.Show("Không thể thực thi câu lệnh Sql này!\nLỗi" + ex.Message, "Lỗi truy vấn", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return result;
         }
     }
 }
