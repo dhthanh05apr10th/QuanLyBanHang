f74ad568452c4b4c6e1316ef0a0d4f4662317c8f
dhthanh05apr10th <dhthanh.05april10th@gmail.com>
2025-11-26 18:59:59 +0700

    Add project files.

---
+++ MyDataTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang
+{
+    internal class MyDataTable
+    {
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        SqlCommand command;
+
+        public string ConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder["Server"] = @".\SQLEXPRESS";
+            builder["Database"] = "QuanLyBanHang";
+            builder["Integrated Security"] = true;
+            return builder.ConnectionString;
+        }
+    }
+}
